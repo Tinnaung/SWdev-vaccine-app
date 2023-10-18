67b5f968a51d2dd89b2d41ecb79f481cc5dc0eda
Tinnaung <6430129921@student.chula.ac.th>
2023-10-18 18:50:07 +0700

    assignment-10

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import styles from "./banner.module.css";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export default function Banner() {
   const covers = [
@@ -13,6 +14,8 @@ export default function Banner() {
   ];
   const [index, setIndex] = useState(0);
   const router = useRouter();
+  const {data:session} = useSession()
+  console.log(session?.user.token)
   return (
     <div
       className={styles.banner}
@@ -29,6 +32,11 @@ export default function Banner() {
           an important part of protecting yourself
         </h3>
       </div>
+      {
+        session? <div className="z-30 absolute top-5 right-10 font-semibold text-yellow-100 text-xl">
+          Hello {session.user?.name}
+        </div> : null
+      }
       <button
         className="bg-white text-cyan-600 border border-cyan-600 
         font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-0 hover:bg-cyan-600 hover:text-white hover:border-transparent"
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,9 +1,13 @@
 import styles from "./topmenu.module.css";
 import Image from "next/image";
 import TopMenuItem from "./TopMenuItem";
-import Link from "next/link";
 
-export default function TopMenu() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { Link } from "@mui/material";
+
+export default async function TopMenu() {
+  const session = await getServerSession(authOptions)
   return (
     <div className="flex flex-row-reverse fixed items-center flex-wrap bg-white border border-y-2 m-0 inset-y-0 h-20 w-full p-2 z-30 ">
       <Link href={"/"}>
@@ -16,9 +20,15 @@ export default function TopMenu() {
           sizes="100vh"
         />
       </Link>
-      {/* <TopMenuItem title="Reservations" pageRef="/reservations" />
-      <TopMenuItem title="About" pageRef="/about" /> */}
       <TopMenuItem title="Booking" pageRef="/booking" />
+      {
+        session? <Link href="/api/auth/signout">
+          <div className="flex items-center absolute left-0 px-2 absolute left-0 text-cyan-600 text-md ml-8">Sign-Out of {session.user?.name}</div>
+        </Link>
+        :<Link href="/api/auth/signin">
+          <div className="flex items-denter absolute left-0 px-2 absolute left-0 text-cyan-600 text-md ml-8">Sign-In</div>
+        </Link>
+      }
     </div>
   );
 }
